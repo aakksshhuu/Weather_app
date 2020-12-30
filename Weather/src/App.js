e7584583a20415e8cba1ee83b30b060105055cbf
Akshit <akshit1172.cse18@chitkara.edu.in>
2020-12-30 12:42:54 +0530

    extra code removed

--- Weather/src/App.js
+++ Weather/src/App.js
@@ -1,21 +1,10 @@
 import React from 'react';
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
-import './App.css';
-import Home from './Home'
 import weather from './weather'
-import todo from './todoComponents/todo'
-import { Switch, Route } from 'react-router-dom';
-import Navbar from './Navbar'
+
 function App() {
   return (
     <>
-    <Navbar />
-    <Switch>
-      <Route path="/todo" component={todo} />
-      <Route path="/weather" component={weather} />
-      <Route path="/" component={Home} />
-    </Switch>
+    <weather />
     </>
   );
 }
